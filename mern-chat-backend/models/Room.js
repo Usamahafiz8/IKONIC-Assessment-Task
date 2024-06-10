35047fb17820c084deac83f07299038b167a8989
usamahafiz8 <usamahafiz8@gmail.com>
2024-06-10 20:07:55 +0500

    uploading basic

--- mern-chat-backend/models/Room.js
+++ mern-chat-backend/models/Room.js
@@ -1,8 +1,7 @@
-
 const mongoose = require('mongoose');
 
 const RoomSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, unique: true, required: true },
   messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
 });
 
